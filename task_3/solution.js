a0ada103276196251c308450935008f1333bdb3d
SPIRT5 <108623559+SPIRT5@users.noreply.github.com>
2022-12-16 11:32:15 +0300

    Add work
    Add work

---
+++ task_3/solution.js
@@ -0,0 +1,18 @@
+function renderCartItem(item) {
+    const product = item;
+
+    // Задание №3.1. Формирование строки корзины
+    // product - объект вида {id: id, title: title, count: count, price: price}
+    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
+    // id - идентификатор
+    // title - наименование
+    // price - цена
+    // count - количество
+
+    let sum = product.count * product.price;
+    let itemCountText = `${product.count} × ${product.price} ₽ = ${sum} ₽`;
+
+    // Конец решения задания №3.1.
+
+    return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
+}
